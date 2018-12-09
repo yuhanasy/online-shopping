406cc5b28abee9d4e158cc1a61f15734f72eade6
Yuhanas Yulianto <yuhanas.yulianto@gmail.com>
2018-12-10 00:40:05 +0700

    able to add item in list

--- src/App.js
+++ src/App.js
@@ -9,16 +9,48 @@ class App extends Component {
     super(props);
 
     this.state = {
-      newItem: [],
       list: [
-        {id: 1, name: 'git', stock: 5},
-        {id: 2, name: 'javascript', stock: 6},
-        {id: 3, name: 'react js', stock: 3},
-        {id: 4, name: 'redux', stock: 7},
-      ]
+        {id: 1, name: 'git', stock: 5, img_src: ''},
+        {id: 2, name: 'javascript', stock: 6, img_src: ''},
+        {id: 3, name: 'react js', stock: 3, img_src: ''},
+        {id: 4, name: 'redux', stock: 7, img_src: ''},
+      ],
+      name: '',
+      stock: 0,
+      img_src: '',
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  handleChange = e => {
+    const key = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [key]: value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let id = Math.random();
+    let newList = {
+      id: id, 
+      name: this.state.name.slice(),
+      stock: this.state.stock.slice(),
+      img_src: this.state.img_src.slice()
+    }
+    console.log(newList)
+
+    let list = [...this.state.list, newList]
+    this.setState({
+      list: list,
+      name: '',
+      stock: 0,
+      img_src: ''
+    })
+  }
 
   render() {
     return (
@@ -56,6 +88,37 @@ class App extends Component {
             })}
           </div>
         </div>
+
+        <div className="container section row">
+          <h3>Add Items</h3>
+          <form className="col s12 margin-top" onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="input-field col l4">
+                <input id="name-item" placeholder="Item's name" name="name" type="text" className="validate" onChange={this.handleChange} value={this.state.name} />
+                <label htmlFor="name-item">Item's Name</label>
+              </div>
+              <div className="input-field col l4">
+                <input id="stock" placeholder="How much your item is?" name="stock" type="number" min="0" className="validate" onChange={this.handleChange} value={this.state.stock} />
+                <label htmlFor="quantity">Item's Quantity</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col l6">
+                <input id="img-src" placeholder="Where is the item's image stored?" name="img_src" type="text" className="validate" onChange={this.handleChange} value={this.state.img_src} />
+                <label htmlFor="img-src">Item's Source Path</label>
+                <span className="helper-text" data-error="wrong" data-success="right">Example: /assets/img/reactjs.png</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s4">
+                <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.handleSubmit}>
+                  Submit
+                  <i className="material-icons right"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>        
       </div>
     );
   }

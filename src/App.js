f709a2a3da37d7781668f7a0aae0f24391525bf8
Yuhanas Yulianto <yuhanas.yulianto@gmail.com>
2018-12-10 10:45:49 +0700

    restructuring component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,103 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css'
+// import M from 'materialize-css'
 
+
+// Navbar Component
+const Navbar = () => {
+  return (
+    <nav className="purple darken-4">
+      <div className="nav-wrapper container">
+        <a href="home.hmtl" className="brand-logo">
+          <img src={logo} className="App-logo" alt="Logo" />
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="sass.html">Your Order</a></li>
+          <li><a href="badges.html">List Buyers</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+// Card Component
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true
+    }
+    this.buyItem = this.buyItem.bind(this)
+  }
+
+  buyItem(itemName) {
+    console.log(itemName);
+    this.setState({
+      hidden: !this.state.hidden
+    })
+    console.log(this.state.hidden)
+  }
+  
+  render() {
+    const hidden = this.state.hidden;
+    const action = hidden ? (
+        <div className="row">
+          <button className="btn waves-effect waves-light purple darken-4" type="submit" name="action" onClick={() => this.buyItem(this.props.title)}>
+            Buy
+            <span><i className="material-icons right">shopping_basket</i></span>
+          </button>  
+        </div>
+      ) : (
+        <div className="row">
+          <button className="btn-small waves-effect waves-light purple darken-3 col s3">
+            <i className="material-icons">remove</i>
+          </button>
+          <input id="input-quantity" placeholder="1" name="input-quantity" type="number" min="0" max={this.props.stock} className="col s2 offset-s1 center-align validate" onChange={this.handleChange} value={this.state.name} />
+          <button className="btn-small waves-effect waves-light purple darken-3 col s3 offset-s1">
+            <i className="material-icons">add</i>
+          </button>                
+        </div>
+      )
+
+    return(
+      <div className="col s12 m4 l3">
+        <div className="card small center-align">
+          <div className="card-image">
+            <div className="img-dummy center-aligned purple lighten-5"></div>
+          </div>
+          <div className="card-title">
+            {this.props.title}
+          </div>
+          <div className="card-action">
+            {action}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+// CardGroup Component
+const CardGroup = (props) => {
+  const itemList = props.list;
+  return(
+    <div className="section container">
+      <div className="row">
+        {itemList.map(item => {
+          return (
+          <Card title={item.name} key={item.id} stock={item.stock} />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+
+
+// App Component
 class App extends Component {
   constructor(props) {
     super(props);
@@ -55,70 +150,45 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="purple darken-4">
-          <div className="nav-wrapper container">
-            <a href="home.hmtl" className="brand-logo">
-              <img src={logo} className="App-logo" alt="Logo" />
-            </a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="sass.html">Your Order</a></li>
-              <li><a href="badges.html">List Buyers</a></li>
-            </ul>
-          </div>
-        </nav>
-        
-        <div className="section container">
-          <div className="row">
-            {this.state.list.map(item => {
-              return(
-                <div className="col s12 m4 l3" key={item.id}>
-                  <div className="card small center-align">
-                    <div className="card-image">
-                      <div className="img-dummy center-aligned purple lighten-5"></div>
-                    </div>
-                    <div className="card-title">
-                      {item.name}
-                    </div>
-                    <div className="card-action">
-                      <button className="btn waves-effect waves-light purple darken-4" type="submit" name="action">Buy</button>
-                    </div>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
+        <Navbar />        
+        <CardGroup list={this.state.list} />
 
+        {/* Form Add Items */}
         <div className="container section row">
           <h3>Add Items</h3>
           <form className="col s12 margin-top" onSubmit={this.handleSubmit}>
             <div className="row">
               <div className="input-field col l4">
-                <input id="name-item" placeholder="Item's name" name="name" type="text" className="validate" onChange={this.handleChange} value={this.state.name} />
                 <label htmlFor="name-item">Item's Name</label>
+                <input id="name-item" placeholder="Item's name" name="name" type="text" className="validate" onChange={this.handleChange} value={this.state.name} />
               </div>
               <div className="input-field col l4">
-                <input id="stock" placeholder="How much your item is?" name="stock" type="number" min="0" className="validate" onChange={this.handleChange} value={this.state.stock} />
                 <label htmlFor="quantity">Item's Quantity</label>
+                <input id="stock" placeholder="How much your item is?" name="stock" type="number" min="0" className="validate" onChange={this.handleChange} value={this.state.stock} />
               </div>
             </div>
             <div className="row">
               <div className="input-field col l6">
-                <input id="img-src" placeholder="Where is the item's image stored?" name="img_src" type="text" className="validate" onChange={this.handleChange} value={this.state.img_src} />
                 <label htmlFor="img-src">Item's Source Path</label>
+                <input id="img-src" placeholder="Where is the item's image stored?" name="img_src" type="text" className="validate" onChange={this.handleChange} value={this.state.img_src} />
                 <span className="helper-text" data-error="wrong" data-success="right">Example: /assets/img/reactjs.png</span>
               </div>
             </div>
             <div className="row">
               <div className="col s4">
-                <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.handleSubmit}>
+                <button className="btn waves-effect waves-light purple darken-4" type="submit" name="action" onClick={this.handleSubmit}>
                   Submit
-                  <i className="material-icons right"></i>
+                  <i className="material-icons right">send</i>
                 </button>
               </div>
             </div>
           </form>
-        </div>        
+        </div>
+          
+          {/* Total Item */}
+        <div>
+
+        </div>
       </div>
     );
   }
